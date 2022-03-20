dc5a6cd394de2f931a0554c0b69ddcbfd9d91068
SIVA KRISHNA YETUKURI <sivakrishna994@gmail.com>
2022-03-19 22:18:01 -0700

    Update __init__.py

--- FlaskSocialBloggingApp/app/__init__.py
+++ FlaskSocialBloggingApp/app/__init__.py
@@ -1,1 +1,11 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from config import config
 
+bootstrap = Bootstrap()
+mail      = Mail()
+moment    = Moment()
+db        = SQLAlchemy()
